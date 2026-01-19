30685c6e69e05a06067f76b87578f1fd9e056261
Aurora Choban <aurorachoban@gmail.com>
2026-01-19 11:41:57 -0700

    update

--- src/ui/programs/ProgramList.tsx
+++ src/ui/programs/ProgramList.tsx
@@ -2,7 +2,6 @@ import { StyleSheet } from "react-native";
 
 export default function ProgramList() {
   // Optional TODO: Sort programs by name before rendering by copying the programs array and sorting the copy
-
   // TODO: Write the tsx code to render a list of ProgramCard components
   // The root component should be a View for the list, and inside of the view we should map over the programs array
   // For each program, render a ProgramCard component, passing the program as a prop
--- src/ui/programs/ProgramsSection.tsx
+++ src/ui/programs/ProgramsSection.tsx
@@ -1,6 +1,6 @@
-
 export default function ProgramsSection() {
   // TODO: Write the tsx code to render a Section component
+
   // Inside the Section, render the ProgramList component
-  return null;
+  return <ProgramsSection />;
 }
