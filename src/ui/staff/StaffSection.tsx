06ddeeb7cf8b3face9eebcf66f061b9b42ab53f3
Jenna Hackett <jenna.hackett96@gmail.com>
2026-01-26 11:42:28 -0700

    StaffSection completed.

--- src/ui/staff/StaffSection.tsx
+++ src/ui/staff/StaffSection.tsx
@@ -1,5 +1,12 @@
+import Section from "../components/Section";
+import StaffList from "./StaffList";
+
 export default function StaffSection() {
   // TODO: Write the tsx code to render a Section component
   // Inside the Section, render the StaffList component
-  return null;
+  return (
+    <Section title="Staff List" subtitle="List of all staff.">
+      <StaffList />
+    </Section>
+  );
 }
