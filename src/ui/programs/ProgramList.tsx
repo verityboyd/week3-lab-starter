91546a03e81df4c378a9df9b12bbaa5ba425ec6b
Aurora Choban <aurorachoban@gmail.com>
2026-01-26 11:01:12 -0700

    Completed ProgramLisr

--- src/ui/programs/ProgramList.tsx
+++ src/ui/programs/ProgramList.tsx
@@ -1,12 +1,29 @@
-import { StyleSheet } from "react-native";
+import { programs } from "@/src/data/programs";
+import { StyleSheet, View } from "react-native";
+import ProgramCard from "./ProgramCard";
 
 export default function ProgramList() {
   // Optional TODO: Sort programs by name before rendering by copying the programs array and sorting the copy
+  const sorted = [...programs].sort((a, b) => {
+    const byName = a.name.localeCompare(b.name);
+    if (byName !== 0) {
+      return byName;
+    }
+    return a.name.localeCompare(b.name);
+  });
+
   // TODO: Write the tsx code to render a list of ProgramCard components
   // The root component should be a View for the list, and inside of the view we should map over the programs array
   // For each program, render a ProgramCard component, passing the program as a prop
   // Remember to set a key prop on each ProgramCard, using the program id (this allows React to track each component efficiently)
-  return null;
+
+  return (
+    <View style={styles.list}>
+      {sorted.map((program) => (
+        <ProgramCard resource={program} key={program.id} />
+      ))}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
